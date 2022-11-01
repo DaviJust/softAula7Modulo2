94f4ae9e9dc49f26fd58dd17754f5db34d620859
Davi <112717111+DaviJust@users.noreply.github.com>
2022-11-01 15:03:26 -0300

    Add files via upload

---
+++ scriptSoftAula7M2.js
@@ -0,0 +1,26 @@
+// Soma
+// function operaArray(arr, func) {
+//     return func(arr);
+//    }
+//    function somaArray(arr) {
+//     var soma = 0;
+//     for (var i=0; i < arr.length; i++) {
+//     soma += arr[i];
+//     }
+//     return soma;
+//    }
+//    console.log(operaArray([1,3,4,], somaArray))
+   
+//Multiplicação
+//    function operaArray(arr, func) {
+//     return func(arr);
+//    }
+//    function somaArray(arr) {
+//     var soma = 0;
+//     for (var i=0; i < arr.length; i++) {
+//     soma = arr[i] * arr[i];
+//     }
+//     return soma;
+//    }
+//    console.log(operaArray([4,4,], somaArray))
+   
